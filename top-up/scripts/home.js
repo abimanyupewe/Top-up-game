e0f3a66362ea644271fdc6f3ceac76e413054394
BimHexsos <131334478+abimanyupewe@users.noreply.github.com>
2025-01-26 14:34:58 +0700

    Add files via upload

---
+++ top-up/scripts/home.js
@@ -0,0 +1,13 @@
+// nav
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
+        } else {
+            navbar.style.backgroundColor = "transparent";
+        }
+    });
+});
+// nav end
